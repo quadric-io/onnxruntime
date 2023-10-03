77c6035b530d57e955e9370152a22f0e1c3d8a0d
Nigel Drego <ndrego@users.noreply.github.com>
2023-10-03 15:54:50 -0700

    QuadricCustomOp: Handle multiple outputs when shape inferencing (#17)

--- onnxruntime/core/graph/contrib_ops/contrib_defs.cc
+++ onnxruntime/core/graph/contrib_ops/contrib_defs.cc
@@ -3043,7 +3043,6 @@ Input absmax is stored in same type as original type of B(float32, float16) with
       .Attr("element_wise", "True (1) if only element-wise ops, False (0) otherwise", AttributeProto::INT, true)
       .TypeConstraint("T", OpSchema::all_tensor_types_with_bfloat(),
                       "Allow inputs and outputs to be any kind of tensor.");
-  // FIXME: Add a type/shape inference function
 
 #ifdef ENABLE_TRAINING_OPS
   // Should remove the shrunken_gather include from ENABLE_TRAINING_OPS once 1). compute optimizer is enabled for inference or
