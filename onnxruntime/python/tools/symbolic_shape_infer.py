562529c30a06e1c0293b5f57050e2809cee42088
Albert Ai <alberta@quadric.io>
2024-10-07 15:59:28 -0700

    Shape inference for opset 18 ReduceMean (#21)
    * Added shape inference for opset 18 ReduceMean
    
    * Refactored out common code between ReduceSum and ReduceMean

--- onnxruntime/python/tools/symbolic_shape_infer.py
+++ onnxruntime/python/tools/symbolic_shape_infer.py
@@ -167,6 +167,7 @@ class SymbolicShapeInference:
             "Reciprocal": self._pass_on_shape_and_type,
             "ReduceSum": self._infer_ReduceSum,
             "ReduceProd": self._infer_ReduceProd,
+            "ReduceMean": self._infer_ReduceMean,
             "Reshape": self._infer_Reshape,
             "Resize": self._infer_Resize,
             "Round": self._pass_on_shape_and_type,
@@ -1589,34 +1590,7 @@ class SymbolicShapeInference:
         keep_dims = get_attribute(node, "keepdims", 1)
         if get_opset(self.out_mp_) >= 13 and len(node.input) > 1:
             # ReduceSum changes axes to input[1] in opset 13
-            axes = self._try_get_value(node, 1)
-            vi = self.known_vi_[node.output[0]]
-            if axes is None:
-                assert keep_dims  # can only handle keep_dims==True when axes is unknown, by generating new ranks
-                vi.CopyFrom(
-                    helper.make_tensor_value_info(
-                        node.output[0],
-                        self.known_vi_[node.input[0]].type.tensor_type.elem_type,
-                        get_shape_from_sympy_shape(self._new_symbolic_shape(self._get_shape_rank(node, 0), node)),
-                    )
-                )
-            else:
-                shape = self._get_shape(node, 0)
-                output_shape = []
-                axes = [handle_negative_axis(a, len(shape)) for a in axes]
-                for i, d in enumerate(shape):
-                    if i in axes:
-                        if keep_dims:
-                            output_shape.append(1)
-                    else:
-                        output_shape.append(d)
-                vi.CopyFrom(
-                    helper.make_tensor_value_info(
-                        node.output[0],
-                        self.known_vi_[node.input[0]].type.tensor_type.elem_type,
-                        output_shape,
-                    )
-                )
+            self._infer_reduce_common(node)
 
     def _infer_ReduceProd(self, node):  # noqa: N802
         axes = get_attribute(node, "axes")
@@ -1625,6 +1599,42 @@ class SymbolicShapeInference:
             data = self._get_int_or_float_values(node)[0]
             if data is not None:
                 self.sympy_data_[node.output[0]] = sympy_reduce_product(data)
+
+    def _infer_ReduceMean(self, node):
+        keep_dims = get_attribute(node, "keepdims", 1)
+        if get_opset(self.out_mp_) >= 18 and len(node.input) > 1:
+            # ReduceMean changes axes to input[1] in opset 18
+            self._infer_reduce_common(node)
+
+    def _infer_reduce_common(self, node):
+        axes = self._try_get_value(node, 1)
+        vi = self.known_vi_[node.output[0]]
+        if axes is None:
+            assert keep_dims  # can only handle keep_dims==True when axes is unknown, by generating new ranks
+            vi.CopyFrom(
+                helper.make_tensor_value_info(
+                    node.output[0],
+                    self.known_vi_[node.input[0]].type.tensor_type.elem_type,
+                    get_shape_from_sympy_shape(self._new_symbolic_shape(self._get_shape_rank(node, 0), node)),
+                )
+            )
+        else:
+            shape = self._get_shape(node, 0)
+            output_shape = []
+            axes = [handle_negative_axis(a, len(shape)) for a in axes]
+            for i, d in enumerate(shape):
+                if i in axes:
+                    if keep_dims:
+                        output_shape.append(1)
+                else:
+                    output_shape.append(d)
+            vi.CopyFrom(
+                helper.make_tensor_value_info(
+                    node.output[0],
+                    self.known_vi_[node.input[0]].type.tensor_type.elem_type,
+                    output_shape,
+                )
+            )
 
     def _infer_RelativePositionBias(self, node):  # noqa: N802
         seq_len = self._try_get_value(node, 1)
