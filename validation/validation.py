dac856cfef4507b887d5b1a1345cab7ee59116cb
Chris Miles <chris@quadric.io>
2025-02-05 16:15:33 -0800

    modified testing infrastructure

--- validation/validation.py
+++ validation/validation.py
@@ -4,41 +4,38 @@ import time
 import os
 import sys
 from tvm.contrib.epu.chimera_job.chimera_job import ChimeraJob
+from qlinearconv import get_onnx
+import onnx
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+RESET = "\033[0m"  # Resets to default color
 
-print(np.__version__)
 def run_ort(flag, x_data, onnx_file_path="/Users/maggies/Desktop/resnet50_512_1024_int8_opset11.onnx"):
     # Create an inference session
     session_options = ort.SessionOptions()
     session_options.enable_gpnpu = flag
-    session_options.enable_profiling = True
+    # session_options.enable_profiling = True
     session_options.intra_op_num_threads = 16
-    session_options.profile_file_prefix = str(16)+"gpnpu"
+    # session_options.profile_file_prefix = str(16)+"gpnpu"
     session = ort.InferenceSession(onnx_file_path, sess_options = session_options, providers=["CPUExecutionProvider"])
     # Inspect the model's input to get the name and shape
     inp_info = session.get_inputs()[0]
     input_name = inp_info.name
-    input_shape = inp_info.shape  # e.g. [1, 8, 128, 128]
-    # print(f"Model input name: {input_name}")- 377
-    # print(f"Model input shape: {input_shape}")
+    input_shape = inp_info.shape
 
     # If any dimension is None or 'batch size' is variable, adjust accordingly
     shape_tuple = tuple(dim if isinstance(dim, int) else 1 for dim in input_shape)
     # Run inference
     output_name = session.get_outputs()[0].name
-    t1 = time.time()
+
     output_data = session.run([output_name], {input_name: x_data})[0]
-    name = session.end_profiling()
-    t2 = time.time()
+    # name = session.end_profiling()
 
-    # print(t2-t1)
-    # Print shapes and types
-    # print(f"Input data shape: {x_data.shape}, dtype: {x_data.dtype}")
-    # print(f"Output data shape: {output_data.shape}, dtype: {output_data.dtype}")
-    # print("Output data (truncated):\n", output_data.flatten()[:50], "...\n")
 
-    return output_data.flatten()
+    return output_data
 
 def run_tvm(img_input, model_path):
     # Execute retina net with CGC
@@ -49,48 +46,50 @@ def run_tvm(img_input, model_path):
 
     outputs = cgc_job.run_inference_harness(inputs={"inp": img_input})
     # return outputs
-    name = list(outputs.keys())[0]
-    return outputs['conv_0.output'].flatten()
+    output_key = list(outputs.keys())[0]
+    return outputs[output_key]
 
 if __name__ == "__main__":
-    # total = 0
-    # n = 1
-    # name = ""
-    # for num in range(4, 20, 4):
-    #     total = 0
-    #     for i in range(n):
-    #         t, name = run_qlinearconv_model(num)
-    #         total += t
+    kernel_size_list = np.arange(2, 10)
+    num_inputs_per_model = 10
+    test_folder = "qlinearconv_test"
+    if not os.path.exists(test_folder):
+        os.makedirs(test_folder)
 
 
-    #     cpu_df, gpu_df = json_to_df(load_json(name), lambda x: True)
-    #     print(str(num) + " - " + str(round(total/n*1000)) + " " + str(round(np.sum(cpu_df["duration"])/1000)))
-    # x_data = np.random.rand(1, 3, 224, 224).astype(np.float32) # resnet 50
-    # x_data = (x_data * 255) - 128
-    # print(x_data)
-    # x_data = np.random.rand(1, 8, 128, 128).astype(np.int8)   # qlinearconv
-    # x_data = np.random.rand(1,2048,7,7) # qlineargap
-    # x_data = np.random.rand(1, 2024)
-    x_data = np.random.rand(1, 8, 128, 128)
-    x_data = (x_data * 255) - 128
-    x_data = x_data.astype(np.int8)
-    shape_tuple = (1,2048,7,7) # qlineargap
-    # shape_tuple = (1,3,224,224) # resnet 50
-    # shape_tuple = (1, 8, 128, 128) # qlinearconv
-    # x_data = np.random.randint(
-    #             low=-128, high=128, size=shape_tuple, dtype=np.int8
-    #         )
+    for kernel_size in kernel_size_list:
+        # create onnx model
+        model_path = f"{test_folder}/qlinearconv_model.onnx"
+        h = 128
+        w = 128
+        ic = 8
+        oc = 64
+        strides = [1, 1]
+        model_def = get_onnx(
+            h,
+            w,
+            ic,
+            oc,
+            kernel_size,
+            strides)
+        onnx.save(model_def, model_path)
+        print(f"\n{GREEN}Running model with kernel size {kernel_size}{RESET}\n")
+        for j in range(num_inputs_per_model):
+            # create input
+            x_data = np.random.rand(1, ic, h, w)
+            x_data = (x_data * 255) - 128
+            x_data = x_data.astype(np.int8)
+            print("\nInput data (first 10 elements):", x_data.flatten()[:10])
+            np.save(f"{test_folder}/input.npy", x_data)
 
-    # print(x_data)
-    # path = "qlinearconv_model.onnx"
-    path = "/Users/maggies/Work/onnxruntime/onnxruntime/test/python/gpnpumode/qlinearconv_model.onnx"
-    output_ort_gpnpu = run_ort(True, x_data, path)
-    output_ort_cpu = run_ort(False, x_data, path)
-    max_diff = np.max(np.abs(output_ort_cpu - output_ort_gpnpu))
-    print(max_diff)
-    np.save("gpnpu.npy", output_ort_gpnpu)
-    np.save("cpu.npy", output_ort_cpu)
-    output_tvm = run_tvm(x_data, path)
-    print(output_tvm)
-    # print(output_tvm.keys())
-    np.save("tvm.npy", output_tvm)
+            # individual runs
+            output_ort_gpnpu = run_ort(True, x_data, model_path)
+            output_ort_cpu = run_ort(False, x_data, model_path)
+            output_tvm = run_tvm(x_data, model_path)
+
+            # compute error
+            max_abs_error_gpnpu_vs_tvm = np.max(np.abs(output_ort_gpnpu-output_tvm))
+            max_abs_error_gpnpu_vs_cpu = np.max(np.abs(output_ort_gpnpu-output_ort_cpu))
+            print(f"\n{BLUE}Processing input {j+1}/{num_inputs_per_model}, GPNPU vs TVM: {max_abs_error_gpnpu_vs_tvm}, GPNPU vs CPU: {max_abs_error_gpnpu_vs_cpu}{RESET}\n")
+            assert max_abs_error_gpnpu_vs_tvm == 0
+            assert max_abs_error_gpnpu_vs_cpu <= 1
