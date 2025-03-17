679ce7e6b28df7779dd9d10427a5c37680ff2cda
Chris Miles <chris@quadric.io>
2025-03-17 16:11:14 -0700

    added test

---
+++ onnxruntime/test/contrib_ops/quantize_linear_fixed_point_test.cc
@@ -0,0 +1,17 @@
+#include "test/providers/compare_provider_test_utils.h"
+
+namespace onnxruntime {
+namespace test {
+
+TEST(QuantizeLinearFixedPointTest, BasicTest) {
+    OpTester tester("QuantizeLinearFixedPoint", 1, kQuadricDomain);
+    tester.AddInput<int32_t>("X", {5}, {-2147483648, 187904816, 268435456, 456340288, 2147483647});
+    tester.AddInput<int8_t>("x_frac_bits", {}, {27});
+    tester.AddInput<float>("scale", {}, {0.01865844801068306});
+    tester.AddInput<int8_t>("zero_point", {}, {-14});
+    tester.AddOutput<int8_t>("Y", {5}, {-128 ,61, 93, 127, 127});
+    tester.Run();
+}
+
+}  // namespace test
+}  // namespace onnxruntime
