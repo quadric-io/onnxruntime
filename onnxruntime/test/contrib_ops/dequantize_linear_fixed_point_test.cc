44d9f6c726eecd375900728e1d9e41bbac153ae9
Chris <chris@quadric.io>
2025-04-07 10:18:45 -0700

    adding dequantizeLinearFixedPoint and quantizeLinearFIxedPoint (#40)
    * adding demo script . adding to track progress, but will remove commit when ready to PR
    
    * all fixedpoint version
    
    * working fixedpoint version in python
    
    * saving
    
    * have python prototype working for dequantizeLinear
    
    * renamed file
    
    * workign tvm script for quantizeLinear
    
    * saving working python prototype of quantizeLinear op
    
    * first version of core implementation of dequantizeLinear
    
    * working version of dequantizeLienar contrip op
    
    * updated dequantizeLinearFixedPoint test
    
    * first working version of quantizeLinearFixedPoint contrib op
    
    * removing onnxruntime/test/python/gpnpumode/test_lutop.py
    
    * removing all python files for manual testing
    
    * changed roundToPosInf so that template arg becomes fucntion arg
    
    * using helper functions used by qlinearconv
    
    * removed debugging print statements and unnecessary comments
    
    * added test
    
    * cleaned up code
    
    * consistent casing
    
    * remove debug statements. fixed typo. removed unnecessary comments
    
    * adding python test for quantizeLinearFixedPoint
    
    * adding python test for dequantizeLinearFixedPoint
    
    * added include <vector>
    
    * corrected test that had overflow since input data provided was out of bounds for choice of frac bits
    
    * modified python test so taht it compares with float version of quantizelinear
    
    * modified python test for dequatnizeLinear so it compares with float version
    
    * just ran formatting on modified sections of code
    
    * updated wheel.yaml to install cmake<4. since the recently released cmake v4 is not yet supported by ort.
    
    * update computeFracBits to match tvm algo

---
+++ onnxruntime/test/contrib_ops/dequantize_linear_fixed_point_test.cc
@@ -0,0 +1,16 @@
+#include "test/providers/compare_provider_test_utils.h"
+
+namespace onnxruntime {
+namespace test {
+
+TEST(DequantizeLinearFixedPointTest, BasicTest) {
+  OpTester tester("DequantizeLinearFixedPoint", 1, kQuadricDomain);
+  tester.AddInput<int8_t>("X", {5}, {-128, 1, 2, 3, 127});
+  tester.AddInput<float>("scale", {}, {0.10242629051208496});
+  tester.AddInput<int8_t>("zero_point", {}, {5});
+  tester.AddOutput<int32_t>("Y", {5}, {-1828407392, -54989696, -41242272, -27494848, 1677185728});
+  tester.Run();
+}
+
+}  // namespace test
+}  // namespace onnxruntime
