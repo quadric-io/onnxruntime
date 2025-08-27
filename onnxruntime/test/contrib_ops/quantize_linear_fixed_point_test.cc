cc116d01a0e2c23d247d8254f212223a9033f844
Chris <chris@quadric.io>
2025-08-27 09:40:46 -0700

    introduced templated version scalarToQfp. Original was defined only single float input and thus this caused a double to be implicitly cast to single. this introduced enough numerical error to differ from CGC output for the case where the round was right around x.5 (#48)

--- onnxruntime/test/contrib_ops/quantize_linear_fixed_point_test.cc
+++ onnxruntime/test/contrib_ops/quantize_linear_fixed_point_test.cc
@@ -29,5 +29,15 @@ TEST(QuantizeLinearFixedPointTest, FourDimTest) {
   tester.Run();
 }
 
+TEST(QuantizeLinearFixedPointTest, ExactMidpointBeforeRound) {
+  OpTester tester("QuantizeLinearFixedPoint", 1, kQuadricDomain);
+  tester.AddInput<int32_t>("X", {1}, {116449696});
+  tester.AddInput<int8_t>("x_frac_bits", {}, {27});
+  tester.AddInput<float>("scale", {}, {0x1.31b3340000000p-6f});
+  tester.AddInput<int8_t>("zero_point", {}, {-14});
+  tester.AddOutput<int8_t>("Y", {1}, {33});
+  tester.Run();
+}
+
 }  // namespace test
 }  // namespace onnxruntime
