9ec5e4ef505510671fd4db1518d397d0e5d40f0a
Chris Miles <chris@quadric.io>
2025-09-12 16:20:54 -0700

    added include

--- onnxruntime/contrib_ops/cpu/quantization/quant_gemm.cc
+++ onnxruntime/contrib_ops/cpu/quantization/quant_gemm.cc
@@ -10,6 +10,7 @@
 #include "core/util/math_cpuonly.h"
 #include "core/util/qmath.h"
 #include "core/mlas/inc/mlas.h"
+#include "core/session/onnxruntime_session_options_config_keys.h"
 
 namespace onnxruntime {
 namespace contrib {
